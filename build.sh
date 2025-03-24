434779345f922ad389fb6756c8cf60e9a7b47453
Ufuk Yavuzer <ufukyavuzer@gmail.com>
2025-03-24 18:04:45 +0300

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Building AD Rest Service executable..."
+python -m PyInstaller adRestService.py -F --windowed --add-data "favicon.ico:ico" --icon "favicon.ico" --distpath Output/dist --workpath Output/build
+echo "Build completed!"
